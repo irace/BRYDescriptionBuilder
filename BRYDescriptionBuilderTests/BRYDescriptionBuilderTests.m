33d2a63e5b418987c767e127a6c604a4bbea2805
Samuel E. Giddins <segiddins@segiddins.me>
2014-03-04 21:39:03 -0600

    Add a really simple test case

--- BRYDescriptionBuilderTests/BRYDescriptionBuilderTests.m
+++ BRYDescriptionBuilderTests/BRYDescriptionBuilderTests.m
@@ -7,8 +7,18 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "BRYDescriptionBuilder.h"
 
 @interface BRYDescriptionBuilderTests : XCTestCase
+
+@end
+
+@interface BRYDescriptionBuilderTestObject : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSNumber *age;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, copy) NSArray *friends;
 
 @end
 
@@ -28,7 +38,27 @@
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    BRYDescriptionBuilderTestObject *object = [BRYDescriptionBuilderTestObject new];
+    object.name = @"Samuel";
+    object.age = @19;
+    object.height = 175.26f;
+    object.friends = @[@"Woody", @"Ernesto"];
+    NSString *expected = [NSString stringWithFormat:@"{\n\t" "super = <%@: %p>" ",\n\t" "name = %@" ",\n\t" "age = %@" ",\n\t" "height = %f" ",\n\t" "friends = %@" "\n}", NSStringFromClass([object class]), object, object.name, object.age, object.height, [object.friends.description stringByReplacingOccurrencesOfString:@"\n" withString:@"\n\t"]];
+    XCTAssertEqualObjects(object.description, expected, @"The description should match the expected value");
+}
+
+@end
+
+@implementation BRYDescriptionBuilderTestObject
+
+- (NSString *)description
+{
+    return [[[[[[BRYDescriptionBuilder builderWithObject:self]
+                appendSuperDescription:[super description]]
+               appendObject:self.name withName:@"name"]
+              appendObject:self.age withName:@"age"]
+             appendFloat:self.height withName:@"height"]
+            appendObject:self.friends withName:@"friends"].description;
 }
 
 @end
